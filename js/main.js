89094ecaf003cc88ec221dcf073533bca73c86e3
Nick Garland <ngarland1@gmail.com>
2014-10-08 19:11:36 -0700

    trying again

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 var app = {
 
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
+
+        this.registerEvents();
+
+        document.addEventListener('deviceready', function() {
+          navigator.splashscreen.hide();
+        });
+
+    },
+
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -28,22 +42,10 @@ var app = {
                 $(event.target).removeClass('tappable-active');
             });
         }
-    },
-
-
-    initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
-        });
-
-        this.registerEvents();
-
-        document.addEventListener('deviceready', function() {
-          navigator.splashscreen.hide();
-        });
-
     }
+
+
+
 
     
 
